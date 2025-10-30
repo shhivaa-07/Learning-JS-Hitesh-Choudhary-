1fe95991cb0a557df4e7f6e3e67a4efe99feb681
Anchal Singh <anchal.shiva994u@gmail.com>
2025-10-31 00:30:08 +0530

    Variables & Datatypes

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId=1712
+let accountEmail="anchal@gmail.com"
+var accountPassword="12345"
+accountCity="Lucknow"
+let accountState;
+
+//remember const value will not change if we do 
+// accountId =2 ...but 
+
+accountEmail="fjfslfj@gmail.com"
+accountPassword="21356473"
+acccountCity="Gurgaon"
+
+/*prefer not to use var 
+because of issue in block scope and fnctional scope*/
+
+console.table([accountId, accountEmail, accountPassword,accountCity,accountState])
+
+
---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all js code as newer version
+//alert(4+4)  //we are using not js not browser
+let accountState;
+let accountEmail="anchal@gmail.com"
+/* string
+number
+boolean
+bigint
+null => It is a standalone value
+undefind=> value not assigned
+symbol=> for uniqueness
+
+object
+*/
+console.log(typeof accountEmail );
+console.log(typeof accountState);
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
