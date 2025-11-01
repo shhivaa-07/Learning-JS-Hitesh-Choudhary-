7b773bd628811177f0e4c7751bbec61f4d564bf3
Anchal Singh <anchal.shiva994u@gmail.com>
2025-11-01 23:05:33 +0530

    Datatypes summary...

---
+++ 01_Basics/05_dataTypeSummary.js
@@ -0,0 +1,19 @@
+//primitive (call by value)
+// Number String Boolean undefined null symbol Bigint
+
+//non primitivez(call by reference)
+//array,objets,functions
+/*  | Value                | typeof Result |
+| -------------------- | ------------- |
+| undefined            | "undefined"   |
+| null                 | "object"      |
+| Boolean values       | "boolean"     |
+| Numbers, NaN         | "number"      |
+| BigInts              | "bigint"      |
+| Strings              | "string"      |
+| Symbols              | "symbol"      |
+| Plain object{}       | "object"      |
+| Array[]              | "object"      |
+| Function             | "function"    |
+| Date, Map, Set, etc. | "object"      |
+  */
